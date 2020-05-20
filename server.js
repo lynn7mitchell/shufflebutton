29117b91e8dfaf18f2fa926aa3ba33ec5af765b1
lynn7mitchell <lynn7mitchell@gmail.com>
2020-05-20 17:29:07 -0500

    changed path.join to path.resolve to try and fix heroku invalid host header

--- server.js
+++ server.js
@@ -17,9 +17,15 @@ if (process.env.NODE_ENV === 'production') {
   }
   // Send every other request to the React app
   // Define any API routes before this runs
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  // });
+  
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-  });
+    res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+});
+
+
 
 http.listen(PORT, function(){
   console.log(`listening on ${PORT}`);
